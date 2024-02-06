5a96c871b578477da48c38370a0d2c466ef4541e
Gustavo Teixeira <contact@gustavoteixeira.dev>
2024-02-06 14:44:18 -0300

    Refactor main.go to handle contact creation and deletion
    This commit refactors `main.go` to introduce the functionality for creating, listing and deleting contacts through HTTP operations. It establishes a Contacts list and methods to manipulate the data, and corresponding routes for GET, POST, DELETE methods. It also introduces some error handling for unique email constraints and invalid contact ids. Changes in `index.html` reflect the new data structures and HTTP endpoints, and CSS files are updated for specific opacity transition effects.

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,8 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 	"html/template"
 	"io"
+	"strconv"
+	"time"
 )
 
 type Templates struct {
@@ -21,24 +23,131 @@ func newTemplate() *Templates {
 	}
 }
 
-type Count struct {
-	Count int
+var id int = 0
+
+type Contact struct {
+	Name  string
+	Email string
+	Id    int
+}
+
+func newContact(name string, email string) Contact {
+	id++
+	return Contact{
+		Name:  name,
+		Email: email,
+		Id:    id,
+	}
+}
+
+type Contacts = []Contact
+
+type Data struct {
+	Contacts Contacts
+}
+
+func (d *Data) indexOf(id int) int {
+	for i, contact := range d.Contacts {
+		if contact.Id == id {
+			return i
+		}
+	}
+	return -1
+}
+
+func newData() Data {
+	return Data{
+		Contacts: []Contact{
+			newContact("John", "Jonhy"),
+			newContact("Clara", "Clarinha"),
+		},
+	}
+}
+
+func (d *Data) hasEmail(email string) bool {
+	for _, contact := range d.Contacts {
+		if contact.Email == email {
+			return true
+		}
+	}
+	return false
+}
+
+type FormData struct {
+	Values map[string]string
+	Errors map[string]string
+}
+
+func newFormData() FormData {
+	return FormData{
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
+type Page struct {
+	Data Data
+	Form FormData
+}
+
+func newPage() Page {
+	return Page{
+		Data: newData(),
+		Form: newFormData(),
+	}
 }
 
 func main() {
 	e := echo.New()
+
+	e.Static("/images", "images")
+	e.Static("/css", "css")
+
 	e.Use(middleware.Logger())
 
+	page := newPage()
 	e.Renderer = newTemplate()
 
-	count := Count{Count: 0}
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", count)
+		return c.Render(200, "index", page)
 	})
 
-	e.POST("/count", func(c echo.Context) error {
-		count.Count++
-		return c.Render(200, "count", count)
+	e.POST("/contacts", func(c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+
+		if page.Data.hasEmail(email) {
+			formData := newFormData()
+			formData.Values["name"] = name
+			formData.Values["email"] = email
+			formData.Errors["email"] = "Email already exists"
+
+			return c.Render(422, "form", formData)
+		}
+		contact := newContact(name, email)
+		page.Data.Contacts = append(page.Data.Contacts, contact)
+
+		err := c.Render(200, "form", newFormData())
+		if err != nil {
+			return err
+		}
+		return c.Render(200, "oob-contact", contact)
+	})
+
+	e.DELETE("/contacts/:id", func(c echo.Context) error {
+		time.Sleep(3 * time.Second)
+		idStr := c.Param("id")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			return c.String(400, "Invalid id")
+		}
+
+		index := page.Data.indexOf(id)
+		if index == -1 {
+			return c.String(404, "Contact not found")
+		}
+		page.Data.Contacts = append(page.Data.Contacts[:index], page.Data.Contacts[index+1:]...)
+		return c.NoContent(200)
 	})
 
 	e.Logger.Fatal(e.Start(":42069"))
